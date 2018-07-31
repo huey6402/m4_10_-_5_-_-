5a0d1e4dcf2691dfef8396b76cd245e4aa6484cd
huey6402 <41720159+huey6402@users.noreply.github.com>
2018-07-31 15:05:19 +0800

    +
    +

--- Program.cs
+++ Program.cs
@@ -12,42 +12,35 @@ class Program
         //  請寫在下面
 
 
-        x = 6;
+        //x = 6;
         // 比如 x 為 6, 則用 2, 3, 4, 5  ( 即比 x 少 1 ) 當除數去除它
         //   , 若都無法整除
         //   , 則其為質數, 可惜2及3當除數去除它, 都可以整除
 
-        y = 7;
+        // y= 7;
         // 比如 y 為 7, 則用 2, 3, 4, 5, 6 ( 即比 y 少 1 ) 當除數去除它
         //   , 若都無法整除
         //   , 則其為質數, 結果真的都無法整除
 
         //Console.WriteLine(y + " 為質數");
 
-        int z , m ;
-        z = 2;
-        m = 6;
+        x = 2;
+        y = 6;
           
-        while(m < 16)
+        while(y < 16)
         {
             
-
-            while (z <= m)
-            {
-
-
-                if (m % z == 0)
-                    break;
-                else
-                    z = z + 1;
-                    if(m == z)
+            while (y % x != 0)
+            {                             
+                    x += 1;
+                    if(x == y)
                     {
-                        Console.WriteLine(m + "為質數");
+                        Console.WriteLine(y + "為質數");
                     }
                 
             }
-        m = m + 1;
-            z = 2;
+        y = y + 1;
+        x = 2;
         }
         Console.WriteLine(); Console.WriteLine();
         Console.WriteLine("結束");
